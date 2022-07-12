bd8ddbb9190812fe22a6534e5cc9eefee6a0fd9b
Jessica M <Prettypuff886@gmail.com>
2022-07-12 04:05:20 -0700

    7-12(1)

---
+++ Dentalia-main/app.js
@@ -0,0 +1,38 @@
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 80,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+
+
+  var swiper = new Swiper(".swiper-2", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+    },
+    slidesPerGroup: 3,
+    loop: true,
+    
+  });
+
+
+  var swiper = new Swiper(".Swiper-3", {
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+    
+  });
+  
